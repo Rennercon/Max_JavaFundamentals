1d281850b28f8cdd2b5c45f90ff0d4fe35261ee2
Maxim <maximklcusursuz@gmail.com>
2023-09-05 16:05:25 +0300

    complete homework nr 1

---
+++ src/main/java/org/example/Main3.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!1");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
